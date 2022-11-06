abd1920273b01d9235c3a735e8849ed3f456b997
Tanvir Hassan <Tanvirtamim72@gmail.com>
2022-11-06 21:03:17 +0600

    core concept
    Text analysis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Context from './componets/Context/Context';
-import Header from './componets/Header/Header';
+import Context from './components/Context/Context';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header  about="About Us"></Header>
+      <Header title="Title" about="About Us"></Header>
       <Context heading="Enter Text Below"></Context>
+      
     </div>
   );
 }
---
+++ src/components/Context/Context.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+
+
+const Context = (props) => {
+    const [text, setText] = useState("");
+
+const addUpHandler =()=>{
+    let newText= text.toUpperCase(text);
+    setText(newText);
+}
+
+const addLowHandler =()=>{
+    let newText= text.toLowerCase(text);
+    setText(newText);
+}
+const addClearHandler =()=>{
+    let newText= '';
+    setText(newText);
+}
+
+const handleCopy=()=>{
+    var text= document.getElementById('myBox');
+    text.select();
+    navigator.clipboard.writeText(text.value);
+}
+const handleExtraSpace=()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(' '));
+}
+const addOnHandler = (event)=>{
+    setText(event.target.value);       
+}
+
+    return (
+        <div>
+            <div className="textArea">
+                
+                
+                <div className="container">
+                        <h1>{props.heading}</h1>
+                        <textarea name="form-control my-1" id="myBox" value={text} onChange={addOnHandler} cols="150" rows="10" placeholder='Enter Your Text Here'></textarea>
+                        <button onClick={addUpHandler} className='btn btn-success me-2'>Convert to UpperCase</button>
+                        <button onClick={addLowHandler} className='btn btn-primary me-2'>Convert to LowerCase</button>
+                        <button onClick={addClearHandler} className='btn btn-warning me-2'>Clear Text</button>
+                        <button onClick={handleCopy} className='btn btn-primary me-2'>Copy Text</button>
+                        <button onClick={handleExtraSpace} className='btn btn-primary me-2'>Remove Extra Space</button>
+                </div>
+
+                <div className="container my-1">
+                    <h2>Text Summary</h2>
+                    <p>{text.split(" ").length} words and {text.length} characters.</p>
+                    <p>{.008 * text.split(" ").length } minutes need to read.</p>
+                    <h2>Preview</h2>
+                    <p>{text}</p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Context;
